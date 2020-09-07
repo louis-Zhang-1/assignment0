eaecf5b2c1412dc5a2ea1c5fa00c4964fcd22dcc
Donghui Zhang <dz26@rice.edu>
2020-09-07 15:49:23 -0500

    initial commit

---
+++ Documents/assignment0/sin.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 15:48:42 2020
+
+@author: loiuszhang
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 15:48:21 2020
+
+@author: loiuszhang
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-10, 10, 0.01)
+y = np.sin(x)
+plt.plot(x, y, color="r", linestyle="-", linewidth=1)
+plt.show()
